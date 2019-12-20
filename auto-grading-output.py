c268d43b4c7537bf78e5f3e5d85444e9af5790bc
Kyle Moran <kylestmoran@gmail.com>
2019-12-19 20:24:05 -0700

    Deleted useless function and linting.

--- auto-grading-output.py
+++ auto-grading-output.py
@@ -3,8 +3,9 @@ import os
 import csv
 import shutil
 import yaml
-from grading_module import *
-import base_config
+import colorama
+from colorama import Style, Fore
+import grading_module as gm
 
 with open("base_config.yml", 'r') as ymlfile:
     cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
@@ -13,22 +14,22 @@ dir = cfg['dir']
 methods = cfg['methods']
 output_only = cfg['output_only']
 check_file = cfg['check_file']
-modify = cfg['modify'] # 1 is a function  2 is modify tests for output test  anything else is regular output
+modify = cfg['modify']  # 1 is a function 2 is modify tests for output test anything else is regular output
 temp_point = input('Points> ')
 if temp_point != '':
-  cfg['point_weight'] = int(temp_point)
-  with open('base_config.yml', 'w') as outfile:
-    yaml.dump(cfg, outfile)
+    cfg['point_weight'] = int(temp_point)
+    with open('base_config.yml', 'w') as outfile:
+        yaml.dump(cfg, outfile)
 point_weight = cfg['point_weight']
 
-validate_dirs(dir)
-solutions = load_solutions(dir['sol'])
-tests = load_tests(dir['test'])
+gm.validate_dirs(dir)
+solutions = gm.load_solutions(dir['sol'])
+tests = gm.load_tests(dir['test'])
 num_correct = 0
 colorama.init()
 print(Style.BRIGHT)
 
-print_tests(solutions, tests)
+gm.print_tests(solutions, tests)
 
 with open('results.csv', 'w', newline='') as csvfile:
     csvwriter = csv.writer(csvfile)
@@ -38,9 +39,9 @@ with open('results.csv', 'w', newline='') as csvfile:
         file_info = lab.split('_')
         if '.py' not in lab:
             continue
-        id = file_info[1] if 'LATE' not in file_info[1] else file_info[2] # print if late
+        id = file_info[1] if 'LATE' not in file_info[1] else file_info[2]  # print if late
         print(Fore.WHITE, f'Here is {file_info[0]}\'s lab:', sep='')
-        grade = grade_script(lab, dir['sub'], tests, solutions, output_only, modify= )
+        grade = gm.grade_script(lab, dir['sub'], tests, solutions, output_only, modify=modify)
         if grade == 1:
             print('success')
             num_correct += 1
